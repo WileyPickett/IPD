337266a472d3fe59d12dabee4584b35965b6aac5
WileyPickett <59930618+WileyPickett@users.noreply.github.com>
2020-01-21 10:05:19 -0900

    Team members
    So basically what we have here is a line  of code that explains oiur three team group members. We are in science class.

--- team3.py
+++ team3.py
@@ -7,6 +7,7 @@
 ####
 
 team_name = 'Big Money' # Only 10 chars displayed.
+#Wiley, Eli, Victor
 strategy_name = 'The name the team gives to this strategy'
 strategy_description = 'How does this strategy decide?'
     
